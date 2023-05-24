654c8fd6e81a47f55786e3b07f9dffd26c4ab027
Ilma-Ameen <ilmaameen31@gmail.com>
2023-05-24 16:23:00 +0500

    code push on github

---
+++ public/app.js
@@ -0,0 +1,10 @@
+var input = document.getElementById("firstname");
+var lastname = document.getElementById("Surname")
+var mobileno = document.getElementById("email") 
+var password = document.getElementById("password") 
+function register(){
+    console.log(input.value);
+    console.log(lastname.value);
+    console.log(mobileno.value);
+    console.log(password.value);
+}
